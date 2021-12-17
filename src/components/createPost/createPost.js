d34e92bc664730d413aad3639d53770d9c0ea9f2
Zeleniuc Vladislav <zeleniuc.vladislav@gmail.com>
2021-12-17 11:17:03 +0200

    trying automatic heroku deploy

--- src/components/createPost/createPost.js
+++ src/components/createPost/createPost.js
@@ -58,7 +58,7 @@ export default function CreatePost({ closeModal }) {
         </label>
         <div className={styles.buttons}>
           <button type="submit" className={styles.submit_btn}>
-            Submit post changed
+            Submit
           </button>
         </div>
       </form>
